a5dee497de47535d3030e4a5682858b57d0b4a1a
Niliem <Niliem111@gmail.com>
2023-09-30 21:15:05 +0300

    Added player state

---
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Player/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+    NetUpdateFrequency = 100.0f;
+}
