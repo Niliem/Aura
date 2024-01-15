72751afaf9ad9dcd58edfe4cb0887bfb41b69b61
Niliem <Niliem111@gmail.com>
2024-01-15 17:30:15 +0300

    91. Aura Ability System Blueprint Library

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemLibrary.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AbilitySystem/AuraAbilitySystemLibrary.h"
+#include "Kismet/GameplayStatics.h"
+#include "UI/HUD/AuraHUD.h"
+#include "UI/WidgetController/OverlayWidgetController.h"
+#include <Player/AuraPlayerState.h>
+
+UOverlayWidgetController* UAuraAbilitySystemLibrary::GetOverlayWidgetController(const UObject* WorldContextObject)
+{
+    if (APlayerController* PlayerController = UGameplayStatics::GetPlayerController(WorldContextObject, 0))
+    {
+        if (AAuraHUD* AuraHUD = Cast<AAuraHUD>(PlayerController->GetHUD()))
+        {
+            if (AAuraPlayerState* PlayerState = PlayerController->GetPlayerState<AAuraPlayerState>())
+            {
+                UAbilitySystemComponent* AbilitySystemComponent = PlayerState->GetAbilitySystemComponent();
+                UAttributeSet* AttributeSet = PlayerState->GetAttributeSet();
+
+                const FWidgetControllerParams WidgetControllerParams(PlayerController, PlayerState, AbilitySystemComponent, AttributeSet);
+
+                return AuraHUD->GetOverlayWidgetController(WidgetControllerParams);
+            }
+        }
+    }
+
+    return nullptr;
+}
---
+++ Source/Aura/Public/AbilitySystem/AuraAbilitySystemLibrary.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "AuraAbilitySystemLibrary.generated.h"
+
+class UOverlayWidgetController;
+
+/**
+ *
+ */
+UCLASS()
+class AURA_API UAuraAbilitySystemLibrary : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+public:
+    UFUNCTION(BlueprintPure, Category = "AuraAbilitySystemLibrary|WidgetController")
+    static UOverlayWidgetController* GetOverlayWidgetController(const UObject* WorldContextObject);
+};
