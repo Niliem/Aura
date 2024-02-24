29e06f7badcce0c1e0d80b83007a633f3b389b08
Niliem <Niliem111@gmail.com>
2024-02-24 14:34:14 +0300

    140. Floating Text Widget

---
+++ Source/Aura/Public/UI/Widget/FloatingTextWidgetComponent.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/WidgetComponent.h"
+#include "FloatingTextWidgetComponent.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class AURA_API UFloatingTextWidgetComponent : public UWidgetComponent
+{
+    GENERATED_BODY()
+
+public:
+    UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+    void SetFloatingTextFromNumber(float Value);
+};
