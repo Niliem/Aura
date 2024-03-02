5330e71988b5fdbbbfe122250a79a8f6bf0c5d84
Niliem <Niliem111@gmail.com>
2024-03-02 15:52:48 +0300

    152. Implementing Net Serialize

--- Source/Aura/Private/AuraAbilityTypes.cpp
+++ Source/Aura/Private/AuraAbilityTypes.cpp
@@ -4,5 +4,32 @@
 
 bool FAuraGameplayEffectContext::NetSerialize(FArchive& Ar, UPackageMap* Map, bool& bOutSuccess)
 {
-    return false;
+    Super::NetSerialize(Ar, Map, bOutSuccess);
+
+    uint8 RepBits = 0;
+    if(Ar.IsSaving())
+    {
+        if(bIsBlockedHit)
+        {
+            RepBits |= 1 << 0;
+        }
+        if (bIsCriticalHit)
+        {
+            RepBits |= 1 << 1;
+        }
+    }
+
+    Ar.SerializeBits(&RepBits, 2);
+
+    if(RepBits & (1 << 0))
+    {
+        Ar << bIsBlockedHit;
+    }
+    if (RepBits & (1 << 1))
+    {
+        Ar << bIsCriticalHit;
+    }
+
+    bOutSuccess = true;
+    return true;
 }
