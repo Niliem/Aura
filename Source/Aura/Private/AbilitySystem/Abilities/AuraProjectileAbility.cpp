fe7897785ea6dd42be2a15fe14e2200ca2f74d17
SolidPenguin <>
2025-05-21 17:54:34 +0300

    316. FireBolt Projectile Spread

--- Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileAbility.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileAbility.cpp
@@ -6,6 +6,8 @@
 #include "AbilitySystemBlueprintLibrary.h"
 #include "Interaction/CombatInterface.h"
 #include "AuraGameplayTags.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Kismet/KismetSystemLibrary.h"
 
 void UAuraProjectileAbility::SpawnProjectile(const FVector& TargetLocation, FGameplayTag Socket, bool bOverridePitch, float PitchOverride)
 {
@@ -33,4 +35,45 @@ void UAuraProjectileAbility::SpawnProjectile(const FVector& TargetLocation, FGam
     Projectile->DamageEffectParams = MakeDamageEffectParamsFromClassDefaults();
 
     Projectile->FinishSpawning(SpawnTransform);
+}
+
+void UAuraProjectileAbility::SpawnProjectiles(const FVector& TargetLocation, FGameplayTag Socket, AActor* HomingTarget, bool bOverridePitch, float PitchOverride)
+{
+    if (!GetAvatarActorFromActorInfo()->HasAuthority())
+        return;
+
+    check(ProjectileActorClass);
+
+    FVector SocketLocation = FVector::ZeroVector;
+    if (GetAvatarActorFromActorInfo()->Implements<UCombatInterface>())
+        SocketLocation = ICombatInterface::Execute_GetCombatSocketLocation(GetAvatarActorFromActorInfo(), Socket);
+    FRotator Rotation = (TargetLocation - SocketLocation).Rotation();
+    if (bOverridePitch)
+    {
+        Rotation.Pitch = PitchOverride;
+    }
+
+    const FVector Forward = Rotation.Vector();
+    const FVector LeftOfSpread = Forward.RotateAngleAxis(-ProjectileSpread / 2.0f, FVector::UpVector);
+    const FVector RightOfSpread = Forward.RotateAngleAxis(ProjectileSpread / 2.0f, FVector::UpVector);
+
+    NumProjectiles = FMath::Min(MaxNumProjectiles, GetAbilityLevel());
+    if (NumProjectiles > 1)
+    {
+        const float DeltaSpread = UKismetMathLibrary::SafeDivide( ProjectileSpread, (NumProjectiles - 1));
+
+        for (int i = 0; i < NumProjectiles; ++i)
+        {
+            const FVector Direction = LeftOfSpread.RotateAngleAxis(DeltaSpread * i, FVector::UpVector);
+            UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), SocketLocation, SocketLocation + Direction * 75.0f, 2.0f, FLinearColor::Red, 100.0f);
+
+        }
+    }
+    else
+    {
+        UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), SocketLocation, SocketLocation + Forward * 75.0f, 2.0f, FLinearColor::Red, 100.0f);
+    }
+    UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), SocketLocation, SocketLocation + Forward * 100.0f, 2.0f, FLinearColor::Green, 100.0f);
+    UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), SocketLocation, SocketLocation + LeftOfSpread * 100.0f, 1.0f, FLinearColor::Gray, 100.0f);
+    UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), SocketLocation, SocketLocation + RightOfSpread * 100.0f, 1.0f, FLinearColor::Gray, 100.0f);
 }
--- Source/Aura/Public/AbilitySystem/Abilities/AuraProjectileAbility.h
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraProjectileAbility.h
@@ -23,6 +23,15 @@ protected:
     TSubclassOf<AAuraProjectileActor> ProjectileActorClass;
 
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Projectile")
+    float ProjectileSpread = 90.0f;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Projectile")
+    int32 NumProjectiles = 1;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Projectile")
+    int32 MaxNumProjectiles = 5;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Projectile")
     bool bUseOverridePitch = false;
 
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (EditCondition = "bUseOverridePitch", EditConditionHides), Category = "Projectile")
@@ -30,4 +39,7 @@ protected:
 
     UFUNCTION(BlueprintCallable, Category = "Projectile")
     void SpawnProjectile(const FVector& TargetLocation, FGameplayTag Socket, bool bOverridePitch = false, float PitchOverride = 0.0f);
+
+    UFUNCTION(BlueprintCallable, Category = "Projectile")
+    void SpawnProjectiles(const FVector& TargetLocation, FGameplayTag Socket, AActor* HomingTarget, bool bOverridePitch = false, float PitchOverride = 0.0f);
 };
